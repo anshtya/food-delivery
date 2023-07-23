d4d0a725575b75de1cb75f113653f945911f91b3
Ansh Tyagi <tyagiansh486@gmail.com>
2023-07-23 17:31:07 +0530

    changed height of BottomNavBar

--- app/src/main/java/com/anshtya/fooddelivery/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/anshtya/fooddelivery/ui/components/BottomNavBar.kt
@@ -3,7 +3,7 @@ package com.anshtya.fooddelivery.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,7 +22,7 @@ fun FoodDeliveryBottomNavBar(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 14.dp)
+            .height(56.dp)
     ) {
         val items = HomeSections.values()
         items.forEach { item ->
