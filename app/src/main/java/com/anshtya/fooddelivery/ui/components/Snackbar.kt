09c688c40637ac6dc1112fe2e49461147423b023
Ansh Tyagi <tyagiansh486@gmail.com>
2023-07-26 18:56:26 +0530

    updated Snackbar design

--- app/src/main/java/com/anshtya/fooddelivery/ui/components/Snackbar.kt
+++ app/src/main/java/com/anshtya/fooddelivery/ui/components/Snackbar.kt
@@ -3,10 +3,12 @@ package com.anshtya.fooddelivery.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
@@ -16,7 +18,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.anshtya.fooddelivery.R
 import com.anshtya.fooddelivery.ui.util.SnackbarData
 
@@ -29,34 +33,46 @@ fun FoodDeliverySnackbar(
 ) {
     SnackbarHost(snackbarHostState) {
         Surface(
-            shape = RoundedCornerShape(5.dp),
-            color = Color.LightGray,
-            modifier = Modifier
+            shape = RoundedCornerShape(10.dp),
+            shadowElevation = 5.dp,
+            modifier = modifier
+                .height(80.dp)
                 .padding(10.dp)
                 .noRippleClickable { onClick() }
         ) {
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = modifier
+                modifier = Modifier
                     .fillMaxWidth()
-                    .padding(10.dp)
+                    .padding(horizontal = 10.dp, vertical = 6.dp)
             ) {
                 Text(
                     stringResource(R.string.total_items_added, snackbarData.totalItems)
                 )
-                Button(
-                    shape = RoundedCornerShape(5.dp),
-                    onClick = onClick
+                Surface(
+                    shape = RoundedCornerShape(10.dp),
+                    color = Color.Red,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .noRippleClickable { onClick() }
                 ) {
                     Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(horizontal = 5.dp)
                     ) {
                         Text(
-                            stringResource(R.string.price, snackbarData.totalPrice)
+                            text = stringResource(R.string.price, snackbarData.totalPrice),
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
                         )
                         Text(
-                            stringResource(R.string.view_cart)
+                            text = stringResource(R.string.view_cart),
+                            color = Color.White,
+                            fontSize = 12.sp
                         )
                     }
                 }
